a20d956f44b74275dc2540856c1bc6c10495d3e6
Kai Chen <kai@sorrentocorp.com>
2017-11-02 16:15:09 -0700

    feat(#494): can run local(login required), prod(not required) mode

--- aleinfo/settings/local.py
+++ aleinfo/settings/local.py
@@ -1,1 +1,15 @@
 from .defaults import *
+
+DEBUG = True
+
+# secret key for local development can be shared
+SECRET_KEY = 'iv3^9^ccmwlfyeyp5q=%#z14p=p^6f1ujyzk8a3p*buzrg^2or'
+
+# perhaps this should be set only for server deployed in private mode
+MIDDLEWARE_CLASSES += (
+    'login.login_middleware.LoginRequiredMiddleware',
+)
+
+INSTALLED_APPS += (
+    'debug_toolbar',
+)
