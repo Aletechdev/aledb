5e8d9a6bbfcaa75476c047453ce5789ec814680e
Kai Chen <kai@sorrentocorp.com>
2017-11-03 11:55:48 -0700

    refact(#425): rename login module ~> accounts before adding logout

--- aleinfo/settings/local.py
+++ aleinfo/settings/local.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'iv3^9^ccmwlfyeyp5q=%#z14p=p^6f1ujyzk8a3p*buzrg^2or'
 
 # perhaps this should be set only for server deployed in private mode
 MIDDLEWARE_CLASSES += (
-    'login.login_middleware.LoginRequiredMiddleware',
+    'accounts.login_middleware.LoginRequiredMiddleware',
 )
 
 INSTALLED_APPS += (
