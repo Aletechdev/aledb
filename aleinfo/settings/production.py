63fe015316c56e3b54ca5901a4293d67972d591f
Lars Schöning <lars@lyschoening.de>
2017-11-13 15:37:12 +0100

    refactor: remove old settings files and scripts

--- aleinfo/settings/production.py
+++
@@ -1,9 +0,0 @@
-from .defaults import *
-
-DEBUG = False
-
-# if we are using os-environ, make sure that we have it set here
-# otherwise, we want a KeyError to be thrown, rather than running
-# production with what could be an unintentinally-set key
-SECRET_KEY = os.environ['SECRET_KEY']
-#SECRET_KEY = 'iv3^9^ccmwlfyeyp5q=%#z14p=p^6f1ujyzk8a3p*buzrg^2or'
